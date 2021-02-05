9c41ab2fe0f12ee3667020d0043a1e790660b0fa
Snehit <snehitchats@gmail.com>
2021-02-05 20:01:00 +0530

    first

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ const express = require('express');
 const app = express();
 var cors = require('cors')
 const mongoose = require('mongoose');
-const PORT = 5000;
+const PORT = process.env.PORT || 5000;
 const {MONGOURI} = require('./keys')
 
 mongoose.connect(MONGOURI, {
